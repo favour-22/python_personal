a7e7c0449bfc4b855eb77572c715ad3dbe2ed5cb
Anointed657@gmail.com <anointed657@gmail.com>
2022-08-25 22:40:24 +0100

    add and mul numbers

---
+++ inputadd.py
@@ -0,0 +1,8 @@
+A = input("Enter a number ")
+B = input("Enter another number ")
+
+sum = int(A) + int(B)
+mul = int(A) * int(B)
+
+print("{} + {} = {}".format(A ,B, sum))
+print("{} * {} = {}".format(A ,B, mul))
